e77b2eed980dc83de30f7857bca6e970934d9bb2
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-05 18:15:30 -0800

    added shipping/billing address form field copy function to checkout-app.js

--- js/checkout-app.js
+++ js/checkout-app.js
@@ -5,3 +5,15 @@ import shoppingCart from './components/shopping-cart.js';
 const cart = cartApi.get();
 
 shoppingCart.init(cart);
+
+
+// Copy shipping address text input to billing input
+var checkbox = document.querySelector('input[name="addr-checkbox"]');
+var shipAddress = document.getElementById('address-1');
+var billAddress = document.getElementById('address-2');
+checkbox.addEventListener('click', function() {
+    if(checkbox.checked) {
+        billAddress.value = shipAddress.value;
+    }
+});
+
